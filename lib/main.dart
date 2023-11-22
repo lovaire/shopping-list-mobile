c7ae7575159a234698d53bfd419cdae9f66bbc04
lovaire <rifsrahmt@gmail.com>
2023-11-22 16:34:50 +0700

    tutorial 9

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:shopping_list/screens/login.dart';
-import 'package:shopping_list/screens/menu.dart';
+
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
